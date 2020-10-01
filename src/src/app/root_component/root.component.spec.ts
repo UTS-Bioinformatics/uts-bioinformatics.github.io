8e7df63e73ab3d5f8ddd9385b928141e9f6f9b59
Jos van Nijnatten <spam@euphemism-inc.nl>
2020-10-01 23:39:00 +0200

    Angular Website initial setup

---
+++ src/src/app/root_component/root.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RootComponent } from './root.component';
+
+describe('RootComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        RootComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(RootComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'website'`, () => {
+    const fixture = TestBed.createComponent(RootComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('website');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(RootComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('website app is running!');
+  });
+});
